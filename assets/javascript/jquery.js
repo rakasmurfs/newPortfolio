ccae9dab56fbda0a65ee62f7347470919a78d25a
Miguel Wolmers <rakasmurfs@gmail.com>
2019-10-15 22:56:21 -0400

    add zoom animation

--- assets/javascript/jquery.js
+++ assets/javascript/jquery.js
@@ -12,23 +12,3 @@ $(".nameHeader").bind("webkitAnimationEnd mozAnimationEnd animationend", functio
   $(".about").hover(function(){
     $(this).addClass("buttonD");        
   })
-  
-  $(document).ready(function () {
-
-    setTimeout(function() {
-      var $rows = $(".project1, .project2, .project4").removeClass("animated");
-
-    }, 1000);
-    setTimeout(function() {
-      var $rows = $(".project3, .project5, .project7").removeClass("animated");
-
-    }, 1500);
-    setTimeout(function() {
-      var $rows = $(".project6, .project8").removeClass("animated");
-
-    }, 2000);
-    setTimeout(function() {
-      var $rows = $(".project9").removeClass("animated");
-
-    }, 2200);
- });
